72d39c073936d5c5e8f41c6101449c933b8440d1
Kazarin <alex464@rambler.ru>
2022-09-09 17:59:02 +0300

    Добавил возможность чтения из файла.

--- src/Test.java
+++ src/Test.java
@@ -15,51 +15,57 @@ class Test {
         String relativePath = "src" + fileSeparator + "input.txt";
         File file = new File(relativePath);
         Scanner sc = new Scanner(file);
-        slovo = sc.nextLine();
-        sc.close();
-        try {
-            a = Double.parseDouble(slovo.split(" ")[0]);
-        }
-        catch (NumberFormatException e) {
-            System.out.print("Error! Not number");
-            flag = false;
-        }
-        try {
-            if (flag) {
-                b = Double.parseDouble(slovo.split(" ")[2]);
-            }
-        }
-        catch (NumberFormatException e) {
-            System.out.print("Error! Not number");
-            flag = false;
-        }
-        if (flag) {
-            if (slovo.split(" ")[1].equals("/") || slovo.split(" ")[1].equals("*") || slovo.split(" ")[1].equals("+") || slovo.split(" ")[1].equals("-")) {
-                znak = slovo.split(" ")[1].charAt(0);
-            } else try {
-                throw new Exception("Operation Error!");
-            } catch (Exception e) {
-                System.out.print(e.getMessage());
+        while (sc.hasNextLine()) {
+            slovo = sc.nextLine();
+
+            try {
+                a = Double.parseDouble(slovo.split(" ")[0]);
+            } catch (NumberFormatException e) {
+                System.out.println("Error! Not number");
                 flag = false;
             }
-        }
-        if (flag){
-            switch(znak){
-                case('+'): System.out.print(a + b);
-                    break;
-                case('-'): System.out.print(a - b);
-                    break;
-                case('*'): System.out.print(a * b);
-                    break;
-                case('/'):
-                    if (b != 0.0) {
-                        System.out.print(a / b);
-                    } else try {
-                        throw new Exception("Error! Division by zero");
-                    } catch (Exception e) {
-                        System.out.print(e.getMessage());
-                    }
-                    break;
+
+            try {
+                if (flag) {
+                    b = Double.parseDouble(slovo.split(" ")[2]);
+                }
+            } catch (NumberFormatException e) {
+                System.out.println("Error! Not number");
+                flag = false;
+            }
+
+            if (flag) {
+                if (slovo.split(" ")[1].equals("/") || slovo.split(" ")[1].equals("*") || slovo.split(" ")[1].equals("+") || slovo.split(" ")[1].equals("-")) {
+                    znak = slovo.split(" ")[1].charAt(0);
+                } else try {
+                    throw new Exception("Operation Error!");
+                } catch (Exception e) {
+                    System.out.println(e.getMessage());
+                    flag = false;
+                }
+            }
+
+            if (flag) {
+                switch (znak) {
+                    case ('+'):
+                        System.out.println(a + " " + znak + " " + b + " = "  + (a + b));
+                        break;
+                    case ('-'):
+                        System.out.println(a + " " + znak + " " + b + " = "  + (a - b));
+                        break;
+                    case ('*'):
+                        System.out.println(a + " " + znak + " " + b + " = "  + (a * b));
+                        break;
+                    case ('/'):
+                        if (b != 0.0) {
+                            System.out.println(a + " " + znak + " " + b + " = "  + (a / b));
+                        } else try {
+                            throw new Exception("Error! Division by zero");
+                        } catch (Exception e) {
+                            System.out.println(e.getMessage());
+                        }
+                        break;
+                }
             }
         }
     }
